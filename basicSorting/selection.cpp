5d5fe4a863ee90ec8e109a1ce47c910b22e0aee9
Bhushan <bhushangothankar2702@gmail.com>
2026-07-24 21:40:04 +0530

    Selection Soting

---
+++ basicSorting/selection.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selection(int arr[] , int n){
+    for(int i = 0;i<=n-2;i++){
+        int mini = i ;
+        for(int j =0;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini = arr[j];
+
+            }
+        }
+        // swap
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+
+}
+int main(){
+    int n ;
+    cin>>n;
+    int arr[n];
+
+    for(int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+
+    }
+    selection(arr,n);
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+        
+    }
+}
